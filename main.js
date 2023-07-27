900016f0593e0db8d08d3cc32bc91a6caa9d3901
Yhur-B <140182636+Yhur-B@users.noreply.github.com>
2023-07-27 11:19:49 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+/*document.getElementById('download'),
+addEventListener('click', function( ) {
+    alert('Download started.....');
+});
+
+document.getElementById('pdf'),
+addEventListener('click', function( ) {
+    alert('Download started.....');
+});
